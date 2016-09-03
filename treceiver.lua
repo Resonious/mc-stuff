7c7ecb481712d99b8838a03b2a4e3afed5eab7dc
Nigel Baillie <metreckk@gmail.com>
2016-09-03 18:03:56 -0400

    fix

--- treceiver.lua
+++ treceiver.lua
@@ -14,12 +14,12 @@ local function render()
   -- Render time
   glass.addBox(x,y,80,10,0xFFFFFF,0.2)
   time = textutils.formatTime(os.time(), false)
-  glass.addText(5,2,"TIME: " .. time, 0xFF0000)
+  glass.addText(x+4,y+1,"TIME: " .. time, 0xFF0000)
   y = y + 12
 
   -- Render turtle state
   glass.addBox(x,y, 100,50, 0xFFFF00, 0.2)
-  glass.addText(5,92, turtleState.message, 0x000000)
+  glass.addText(x+4,y+1, turtleState.message, 0x000000)
 
   glass.sync()
 end
